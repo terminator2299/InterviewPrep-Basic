1f8f2e36e8ee9e1e4e51c9b731cd4963a817a049
terminator2299 <bhavya.code18@gmail.com>
2025-07-05 11:31:02 +0530

    Adding more basic questions

---
+++ FindMaxElement.py
@@ -0,0 +1,6 @@
+def find_max(arr):
+    max_val = arr[0]
+    for num in arr:
+        if num>max_val:
+            max_val = num
+    return max_val
---
+++ PalindromeStringCheck.py
@@ -0,0 +1,6 @@
+def palindrome(s):
+    for i in range(len(s)):
+        if s[i] != s[len(s) - 1 - i]:
+            return False
+    return True
+

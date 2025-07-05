4f4f049aa4cd06ba13b1a1b0406fa0230be2f925
terminator2299 <bhavya.code18@gmail.com>
2025-07-05 11:46:19 +0530

    String basic question

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,10 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+
+    strs.sort()
+    first, last = strs[0], strs[-1]
+    i = 0
+    while i < len(first) and i < len(last) and first[i] == last[i]:
+        i += 1
+    return first[:i]

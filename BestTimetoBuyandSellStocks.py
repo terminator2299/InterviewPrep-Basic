4fa9b8cfea337c01b2e83a6266bab820359753d1
terminator2299 <bhavya.code18@gmail.com>
2025-07-06 22:21:06 +0530

    First question of leetcode

---
+++ BestTimetoBuyandSellStocks.py
@@ -0,0 +1,8 @@
+def maxProfit(prices):
+    buy_price = prices[0]
+    profit = 0
+    for p in prices[1:]:
+        if buy_price > p:
+            buy_price = p
+        profit = max(profit, p - buy_price)
+    return profit

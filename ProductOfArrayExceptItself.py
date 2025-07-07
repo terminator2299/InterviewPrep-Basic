57a4f2bbc73415f56f06ca7bedeaec6d8c2db8a0
terminator2299 <bhavya.code18@gmail.com>
2025-07-07 14:57:40 +0530

    Repeated question in interviews

---
+++ ProductOfArrayExceptItself.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+	def productExceptSelf(self, nums: List[int]) -> List[int]:
+		n = len(nums)
+		ans = [0] * n
+
+		for i in range(n):
+			prod = 1
+			for j in range(n):
+				if i != j:
+					prod *= nums[j]
+			ans[i] = prod
+		return ans
